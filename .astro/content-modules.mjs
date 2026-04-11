5199f13726618c4a116bcaa0c9f61a47e64c080b
zzming <zzming@qq.com>
2026-04-11 18:52:47 +0800

    添加 cloudflare adapter for astro SSR

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,6 @@
+
+export default new Map([
+["src/content/posts/first-post.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Ffirst-post.mdoc&astroContentModuleFlag=true")],
+["src/content/posts/dddd.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fdddd.mdoc&astroContentModuleFlag=true")],
+["src/content/posts/asd-fjadsjfk.mdoc", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fasd-fjadsjfk.mdoc&astroContentModuleFlag=true")]]);
+		
